eeb2d57d5fa669feca387933d2bf25539a46b868
comunnidadoscar@gmail.com <comunidadoscar@gmail.com>
2018-08-13 15:07:36 -0400

    added sign in

---
+++ data.js
@@ -0,0 +1,68 @@
+let students = {
+    Oscar:{
+    
+    firstName: "Oscar",
+    lastName: "Comunidad",
+    grade: 11,
+    class: 2020,
+
+    classes:[
+        {
+            subject:"Algebra II",
+            average: 70 ,
+            room: 503, 
+            teacher: "Ms.Bacchus",
+            period: "1st",
+        },
+
+        {
+            subject:"Gym",
+            average: 95 ,
+            room: 305, 
+            teacher: "Mr.Lymanchek",
+            period: "2nd",
+        },
+
+        {
+            subject:"English",
+            average: 91 ,
+            room: 400, 
+            teacher: "Mr.Sanders",
+            period: "3rd",
+        },
+
+        {    
+            subject:"Financial",
+            average: 90 ,
+            room: 520, 
+            teacher: "Mr.Graham",
+            period: "5th",        
+        },
+
+        {
+            subject:"Lunch",
+            average: "N/A" ,
+            room: 100, 
+            teacher: "N/A",
+            period: "6th",
+        },
+        
+        {
+            subject:"Global II",
+            average: 81,
+            room: 600, 
+            teacher: "Mr.Eduoard",
+            period: "7th",
+        },
+
+        {
+            subject:"Chemistry",
+            average: 91 ,
+            room: 629, 
+            teacher: "Ms.Lyons",
+            period: "8th",
+        },
+    ]
+    }
+    
+}
---
+++ main.js
@@ -0,0 +1,100 @@
+const database = firebase.database().ref();
+let username;
+let email;
+let password;
+let user;
+
+function signUp(event){
+    username = $("#signupUsername").val();
+    password = $("#signupPassword").val();
+    email = $("#email").val();
+    $("#signupUsername").val("");
+    $("#signupPassword").val("");
+    $("#email").val("");
+    //Update database here
+    
+    let json;
+
+    if(email != "" && email.includes("@") && email.includes(".com")){
+        json = {
+            USERNAME: username,
+            PASSWORD: password,
+            EMAIL: email
+        }
+        firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // ...
+          });
+    }else{
+        json = {
+            USERNAME: username,
+            PASSWORD: password,
+        }
+    }
+    database.push(json);
+}
+
+$("#logIn").click(function(){
+    if($("#username").val() !== "" && $("#password").val() !== ""){
+        if($("#username").val().includes(".com")){
+            email = $("#username").val();
+            username = "";
+        }else{
+            username = $("#username").val();
+            email = "";
+        }
+        password = $("#password").val();
+
+        database.once('value', function(snapshot) {
+            let i = 0;
+            snapshot.forEach(function(childSnapshot) {
+                var childData = childSnapshot.val();
+                if(childData.USERNAME == username && childData.PASSWORD == password){
+                    i = 1;
+                    user = username;
+                    alert("Logged In");
+                    window.open("home.html", "_self");
+                }else if(childData.EMAIL == email && childData.PASSWORD == password){
+                    firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+                        // Handle Errors here.
+                        var errorCode = error.code;
+                        var errorMessage = error.message;
+                        // ...
+                      });
+                      i = 1;
+                      alert("Logged In");
+                      window.open("home.html", "_self");
+                }
+            });
+            if(i == 0){
+                alert("Incorrect username or password");
+            }
+        });
+    }else{
+        alert("");
+    }
+});
+
+$("#signUp").click(function(){
+    if($("#signupUsername").val() !== "" && $("#signupPassword").val() !== ""){ 
+        database.once('value', function(snapshot) {
+            let i = 0;
+            snapshot.forEach(function(childSnapshot) {
+                var childData = childSnapshot.val();
+                if($("#signupUsername").val() == childData.USERNAME){
+                    i = 1;
+                    alert("Username Taken");
+                }
+                if($("#email").val().includes(".com") && $("#email").val().includes("@") || $("#email").val() == ""){}else{
+                    alert("Invalid Email");
+                }
+            });
+            if(i == 0){
+                signUp();
+                window.open("index.html", "_self");
+            }
+        });
+    }
+});
