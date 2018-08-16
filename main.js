2aeb33d0e74adeaba2d3982b4605d6e82f452ca7
comunnidadoscar@gmail.com <comunidadoscar@gmail.com>
2018-08-16 14:17:04 -0400

    second test

--- main.js
+++ main.js
@@ -136,64 +136,64 @@ $("#teacherSignUp").click(function(){
     signUpButton("teacher");
 });
 
-$(window).load("home.html", function(){
-    users.once('value', function(snapshot){
-        snapshot.forEach(function(childSnapshot){
-            var childData = childSnapshot.val();
-            if(childData.EMAIL == firebase.auth().currentUser.email){
-                let type = childData.TYPE;
-                if(type == "Student"){
-                    //pastes name
-                    let word = childData.USERNAME; 
-                    $("#welcome").append("<p> Welcome " + word + "!</p>");
-                    // pastes schedule for specific user
+// $(window).load("home.html", function(){
+//     users.once('value', function(snapshot){
+//         snapshot.forEach(function(childSnapshot){
+//             var childData = childSnapshot.val();
+//             if(childData.EMAIL == firebase.auth().currentUser.email){
+//                 let type = childData.TYPE;
+//                 if(type == "Student"){
+//                     //pastes name
+//                     let word = childData.USERNAME; 
+//                     $("#welcome").append("<p> Welcome " + word + "!</p>");
+//                     // pastes schedule for specific user
                         
-                    for(let x = 0; x <  8; x++){
+//                     for(let x = 0; x <  8; x++){
                             
-                        $("#board").append(
-                        "<div class = 'class'>" + 
+//                         $("#board").append(
+//                         "<div class = 'class'>" + 
                             
-                        "<p>Mr.Teacher</p>" +
+//                         "<p>Mr.Teacher</p>" +
 
-                        "<p>Subject</p>"+
+//                         "<p>Subject</p>"+
 
-                        "<p>Period #</p>"+
+//                         "<p>Period #</p>"+
                         
-                        "<p>Grade Average</p>"+
+//                         "<p>Grade Average</p>"+
 
-                        "<p>Room #</p>"
+//                         "<p>Room #</p>"
 
-                        + "</div>");
+//                         + "</div>");
                             
-                    }
-                }else if(type == "Teacher"){
-                    //pastes name
-                    let word = childData.USERNAME; 
-                    $("#welcome").append("<p> Welcome " + word + "!</p>");
-                    $("#title").html("Class List");
+//                     }
+//                 }else if(type == "Teacher"){
+//                     //pastes name
+//                     let word = childData.USERNAME; 
+//                     $("#welcome").append("<p> Welcome " + word + "!</p>");
+//                     $("#title").html("Class List");
 
-                    for( let x = 0; x <  8; x++){
-                        $("#Board").append(
-                            "<div class = 'class'>" + 
+//                     for( let x = 0; x <  8; x++){
+//                         $("#Board").append(
+//                             "<div class = 'class'>" + 
                                 
-                            "<p>Student's name</p>" +
+//                             "<p>Student's name</p>" +
         
-                            "<p>Grade (9th,10th,11th ECT)</p>"+
+//                             "<p>Grade (9th,10th,11th ECT)</p>"+
         
-                            "<p>Period #</p>"+
+//                             "<p>Period #</p>"+
                                 
-                            "<p>Grade Average</p>"+
+//                             "<p>Grade Average</p>"+
         
-                            "<p>Student ID</p>"
+//                             "<p>Student ID</p>"
         
-                            + "</div>");
-                    }
+//                             + "</div>");
+//                     }
 
-                }
-            }
-        });
-    });
-})
+//                 }
+//             }
+//         });
+//     });
+// })
 
 $("#logOut").click(function(){
     firebase.auth().signOut();
