985694b30f3e70e518f7de2e076b064b6a3a9d98
comunnidadoscar@gmail.com <comunidadoscar@gmail.com>
2018-08-16 14:03:19 -0400

    test

--- main.js
+++ main.js
@@ -137,62 +137,62 @@ $("#teacherSignUp").click(function(){
 });
 
 $(window).load("home.html", function(){
-        users.once('value', function(snapshot){
-            snapshot.forEach(function(childSnapshot){
-                var childData = childSnapshot.val();
-                if(childData.EMAIL == firebase.auth().currentUser.email){
-                    let type = childData.TYPE;
-                    if(type == "Student"){
-                        //pastes name
-                        let word = childData.USERNAME; 
-                        $("#welcome").append("<p> Welcome " + word + "!</p>");
-                        // pastes schedule for specific user
+    users.once('value', function(snapshot){
+        snapshot.forEach(function(childSnapshot){
+            var childData = childSnapshot.val();
+            if(childData.EMAIL == firebase.auth().currentUser.email){
+                let type = childData.TYPE;
+                if(type == "Student"){
+                    //pastes name
+                    let word = childData.USERNAME; 
+                    $("#welcome").append("<p> Welcome " + word + "!</p>");
+                    // pastes schedule for specific user
                         
-                        for(let x = 0; x <  8; x++){
+                    for(let x = 0; x <  8; x++){
                             
-                            $("#Board").append(
-                            "<div class = 'class'>" + 
+                        $("#board").append(
+                        "<div class = 'class'>" + 
                             
-                            "<p>Mr.Teacher</p>" +
+                        "<p>Mr.Teacher</p>" +
 
-                            "<p>Subject</p>"+
+                        "<p>Subject</p>"+
 
-                            "<p>Period #</p>"+
+                        "<p>Period #</p>"+
+                        
+                        "<p>Grade Average</p>"+
+
+                        "<p>Room #</p>"
+
+                        + "</div>");
                             
-                            "<p>Grade Average</p>"+
-
-                            "<p>Room #</p>"
-
-                            + "</div>");
-                            
-                        }
-                    }else if(type == "Teacher"){
-                        //pastes name
-                        let word = childData.USERNAME; 
-                        $("#welcome").append("<p> Welcome " + word + "!</p>");
-                        $("#title").html("Class List");
-
-                        for( let x = 0; x <  8; x++){
-                        $("#Board").append(
-                                "<div class = 'class'>" + 
-                                
-                                "<p>Student's name</p>" +
-        
-                                "<p>Grade (9th,10th,11th ECT)</p>"+
-        
-                                "<p>Period #</p>"+
-                                
-                                "<p>Grade Average</p>"+
-        
-                                "<p>Student ID</p>"
-        
-                                + "</div>");
-                        }
-
                     }
+                }else if(type == "Teacher"){
+                    //pastes name
+                    let word = childData.USERNAME; 
+                    $("#welcome").append("<p> Welcome " + word + "!</p>");
+                    $("#title").html("Class List");
+
+                    for( let x = 0; x <  8; x++){
+                        $("#Board").append(
+                            "<div class = 'class'>" + 
+                                
+                            "<p>Student's name</p>" +
+        
+                            "<p>Grade (9th,10th,11th ECT)</p>"+
+        
+                            "<p>Period #</p>"+
+                                
+                            "<p>Grade Average</p>"+
+        
+                            "<p>Student ID</p>"
+        
+                            + "</div>");
+                    }
+
                 }
-            });
+            }
         });
+    });
 })
 
 $("#logOut").click(function(){
